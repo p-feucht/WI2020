2bc992eeef68cbc0dd65d293eea0676eaa639e98
kmatysova <kmatysova2@gmail.com>
2020-10-15 18:43:17 +0200

    added search field and card

--- Teams/Worker-Bees/Kati/categoriesTabFunctions.js
+++ Teams/Worker-Bees/Kati/categoriesTabFunctions.js
@@ -1,6 +1,7 @@
 function openPage(pageName, elmnt) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
+    var color =  "#ffffff00";
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
@@ -10,14 +11,31 @@ function openPage(pageName, elmnt) {
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "";
+      tablinks[i].style.color = "#FFFFFF";
+      tablinks[i].style.fontWeight = "400";
+      tablinks[i].style.textShadow = "none";
     }
   
     // Show the specific tab content
     document.getElementById(pageName).style.display = "block";
   
     // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = backgroundColor;
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = "#feca57";
+    elmnt.style.fontWeight = "800";
+    elmnt.style.textShadow = "2px 2px #000000";
+}
+
+function openDefault() {
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+}
+
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
