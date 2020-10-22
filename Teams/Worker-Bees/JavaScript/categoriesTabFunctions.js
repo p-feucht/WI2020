0dba6b4fc3301d4f9c9af0014d21f428b5e8a4c4
kmatysova <kmatysova2@gmail.com>
2020-10-22 18:10:38 +0200

    search bar added

--- Teams/Worker-Bees/JavaScript/categoriesTabFunctions.js
+++ Teams/Worker-Bees/JavaScript/categoriesTabFunctions.js
@@ -6,52 +6,72 @@ function openPage(pageName, elmnt) {
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-      tablinks[i].style.color = "#FFFFFF";
-      tablinks[i].style.fontWeight = "400";
-      tablinks[i].style.textShadow = "none";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-    elmnt.style.color = "#feca57";
-    elmnt.style.fontWeight = "800";
-    elmnt.style.textShadow = "2px 2px #000000";
 
-    // change hash
-    location.hash = "#" + elmnt.id;
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "#FFFFFF";
+    tablinks[i].style.fontWeight = "400";
+    tablinks[i].style.textShadow = "none";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+  elmnt.style.color = "#feca57";
+  elmnt.style.fontWeight = "800";
+  elmnt.style.textShadow = "2px 2px #000000";
+
+  // change hash
+  location.hash = "#" + elmnt.id;
 }
 
 function openTab() {
 // Get the element with id="defaultOpen" and click on it
 var hash = window.location.hash.substring(1);
 if (hash == 'Werkzeug-Ang') {
-  var elmnt = document.getElementById(hash);
-  openPage('Werkzeug',elmnt);
+var elmnt = document.getElementById(hash);
+openPage('Werkzeug',elmnt);
 }
 else if (hash == 'Werkstatt-Ang') {
-  var elmnt = document.getElementById(hash);
-  openPage('Werkstatt',elmnt);
+var elmnt = document.getElementById(hash);
+openPage('Werkstatt',elmnt);
 }
 else if (hash == 'Dienst-Ang') {
-  var elmnt = document.getElementById(hash);
-  openPage('Dienst',elmnt);
+var elmnt = document.getElementById(hash);
+openPage('Dienst',elmnt);
 }
 
 }
 
 function openSearch() {
-  document.getElementById("myOverlay").style.display = "block";
+document.getElementById("myOverlay").style.display = "block";
 }
 
 // Close the full screen search box
 function closeSearch() {
-  document.getElementById("myOverlay").style.display = "none";
+document.getElementById("myOverlay").style.display = "none";
+}
+
+
+function datePicker() {
+
+  $('input[name="datefilter"]').daterangepicker({
+      autoApply: true,
+      autoUpdateInput: false,
+      locale: {
+          cancelLabel: 'Clear'
+      }
+  });
+
+  $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+  });
+
+  $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+  })
 }
