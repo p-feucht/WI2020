9778009b37915f0deabf9751eab79d20c6356d54
Arfarin <Engelmayer@stud.dhbw-ravensburg.de>
2020-10-27 14:18:05 +0100

    daterangepicker in form for offers works

---
+++ Teams/Worker-Bees/JavaScript/formular.js
@@ -0,0 +1,45 @@
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
+
+function datePicker() {
+
+    $('input[name="datefilter"]').daterangepicker({
+        autoApply: true,
+        autoUpdateInput: false,
+        multidate: true,
+        locale: {
+            cancelLabel: 'Clear'
+        }
+    });
+
+    $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    });
+
+    $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    })
+}
+
+
+/*
+
+function datePicker() {
+    $('.datefilter').datepicker({
+        multidate: true,
+        format: 'dd-mm-yyyy'
+    });
+}*/
