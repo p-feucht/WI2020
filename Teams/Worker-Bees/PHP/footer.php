99e36b846d976f119a5458aca3b3e32dfecf7990
kmatysova <kmatysova2@gmail.com>
2020-10-29 16:40:18 +0100

    added files

---
+++ Teams/Worker-Bees/PHP/footer.php
@@ -0,0 +1,36 @@
+
+    <!-- Outsourced footer PHP-file.-->
+
+    <head>
+
+        <!-- insert style sheet and media contents -->
+        <link href="CSS/footer.css" rel="stylesheet">
+        <link rel="icon" href="images/logoSchriftzug.png" />
+    
+    <head>
+
+    <!-- Footer -->
+    <div class="footer">
+        <div class="footer-top">
+        <div class="footer-left">
+            <img src="images/logoSchriftzug.png" class="logo" alt="Worker Bees Logo">
+            <p class="footer_slogan"> Come to craft</p><br>
+            <a href="mailto:support@workerbees.com">support@workerbees.com</a>
+
+        </div>
+            <div class="footer-right">
+                <p class="#contact">
+                    WorkerBees e.V.<br>
+                    Musterstraße 25<br>
+                    86712 Musterstadt<br>
+                    <br> +49 172 906 212
+                </p>
+            </div>
+        </div>
+    </div>
+
+        <div class="footer_end">
+            <p>Copyright 2020 | WorkerBees e.V.</p>
+        </div>
+
+    </div>
