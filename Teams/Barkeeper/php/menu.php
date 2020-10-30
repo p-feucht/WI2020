7a529079717beacf6353427321515eced6a11c11
siggn <niklas.sigg0804@gmail.com>
2020-10-30 15:10:08 +0100

    container angepasst

--- Teams/Barkeeper/php/menu.php
+++
@@ -1,8 +0,0 @@
-<div id="menu">
-    <ul class="horizontal">
-        <li><a href="html/eingabe.html">Eingabe Lebensmittel</a></li>
-        <li><a href="html/rezepte.html">Rezepte</a></li>
-        <li><a href="html/rezeptidee.html">Rezeptidee</a></li>
-        <li><a href="html/tipps.html">Tipps und Tricks</a></li>
-    </ul>
-</div>
