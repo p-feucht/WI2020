1ff74eff23c5404ac0d118a825a5ec98ed52c996
kmatysova <kmatysova2@gmail.com>
2020-10-31 20:36:43 +0100

    modals fitted to cards in categories

--- Teams/Worker-Bees/PHP/getOffers.php
+++ Teams/Worker-Bees/PHP/getOffers.php
@@ -11,25 +11,86 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Ort, ATitel, PreisProTag FROM AngebotWerkzeug";
+$sql = "SELECT ATitel, AZeitraum, ABeschreibung, Vorname, Nachname, Strasse, Hausnummer, 
+PLZ, Ort, Bild, usernameErsteller, Werkzeug_ID, PreisProTag, BezInBier, Erstellzeitpunkt FROM AngebotWerkzeug";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
 
+        $orderID = $row["Werkzeug_ID"];
         $location = $row["Ort"];
         $title = $row["ATitel"];
         $price = $row["PreisProTag"];
+        $offeruser = $row["usernameErsteller"];
+        $plz = $row["PLZ"];
+        $description = $city = $row["ABeschreibung"];
 
+        $card_ID = "card_".(string)$orderID;
+        $modal_target = "#modal_".(string)$orderID;
+        $modal_ID = "modal_".(string)$orderID;
 ?>
 
-        <div class="card" data-toggle="modal" data-target="#myModal">
+        <!-- create card for each offer -->
+        <div class="card" id=<?php echo $card_ID ?> data-toggle="modal" data-target=<?php echo $modal_target ?>>
             <img src="images/werkstatt.jpg" alt="Denim Jeans" class="offer-image">
             <p class="card-lp"><img src="images/place-icon.svg" alt="location" class="place-icon"> <?php echo $location ?>
                 <span class="price"><?php echo $price ?>€ / Tag</span></p>
             <h2><?php echo $title ?></h1>
         </div>
+
+        <!-- create modal for each card -->
+        <div id=<?php echo $modal_ID ?> class="modal fade" role="dialog">
+        <div class="modal-dialog modal-lg">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <img class="headerLogo" src="images/logoKomplett.png">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <h3 class="modal-offerName"><?php echo $title ?></h3>
+                    <p class="modal-namelocation"><?php echo $offeruser ?>
+                        <img src="images/place-icon.svg" class="place-icon" alt="location"><?php echo $plz ?> <?php echo $location ?>
+                        <!--has to be the exact location here!-->
+                    </p>
+
+                    <div class="modal-content-split">
+                        <p class="offer-description">
+                            <?php echo $description ?>
+                        </p>
+                        <!--only for Werkstat!!
+                        <ul class="modal-amenities">
+                            <li>Standbohrmaschine</li>
+                            <li>elektrische Standsägen</li>
+                        </ul>
+                        -->
+                    </div>
+                    <div class="modal-content-split">
+                        <img src="images/Werkzeug.jpg" class="modal-image" alt="Angebot">
+                    </div>
+
+                    <form class="modal-booking-window">
+                        <h3 class="modal-booking-heading">Nur noch ein Schritt!</h3>
+                        
+                        <p class="modal-booking-text">Gesamtbetrag: 1000 €<br>
+                            <input type="checkbox" name="bierZahlung">
+                            <label for="bierZahlung"> Ich möchte in Bier bezahlen (1000 Bier)</label><br>
+                        </p>
+
+                        <button type="button" class="submitBooking">Jetzt buchen</button>
+                    </form>
+
+
+
+                </div>
+                <div class="modal-footer"></div>
+            </div>
+
+        </div>
+    </div>
 
 <?php
     }
--- Teams/Worker-Bees/categories.php
+++ Teams/Worker-Bees/categories.php
@@ -55,7 +55,7 @@
             <input type="text" id="free-text" placeholder="Was suchst du?">
             <script src="JavaScript/locationSearch.js"></script>
             <input id="autocomplete" class="controls" type="text" placeholder="Wo brauchst du es?" />
-            <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUrJoGLdsBz5xPFlFs-RiG2TPTnELEfOk&libraries=places&callback=initAutocomplete"></script>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUrJoGLdsBz5xPFlFs-RiG2TPTnELEfOk&libraries=places&callback=initAutocomplete"></script>
             <div class="icon"><i class="fa fa-calendar fa-2x" aria-hidden="true"></i></div>
             <input type="text" name="datefilter" value="" placeholder="Wann passt es dir am besten?" />
             <script type="text/javascript" src="JavaScript/categoriesFunctions.js">
@@ -77,61 +77,6 @@
     <div id="Dienst" class="tabcontent">
         <h3>Contact</h3>
         <p>Get in touch, or swing by for a cup of coffee.</p>
-    </div>
-
-    <!-- Pop up window with detailed information and booking option -->
-    <div id="myModal" class="modal fade" role="dialog">
-        <div class="modal-dialog modal-lg">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <img class="headerLogo" src="images/logoKomplett.png">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <h3 class="modal-offerName">Bester Schraubenschlüssel in Town.</h3>
-                    <p class="modal-namelocation">katomato95
-                        <img src="images/place-icon.svg" class="place-icon" alt="location">Genaue Adresse
-                        <!--has to be the exact location here!-->
-                    </p>
-
-                    <div class="modal-content-split">
-                        <p class="offer-description">
-                            This is the best Schraubenschlüssel you have ever seen. Come to my dark garage to pick it up.
-                            Do not come between 1 and 2 pm because that's when I feed my dog and watch it eat.
-                            You can keep the Schraubenschlüssel as long as you want, but I dare you to bring it back broken.
-                            Have a nice day,
-                            Klaus
-                        </p>
-                        <ul class="modal-amenities">
-                            <!--only for Werkstat!!-->
-                            <li>Standbohrmaschine</li>
-                            <li>elektrische Standsägen</li>
-                        </ul>
-                    </div>
-                    <div class="modal-content-split">
-                        <img src="images/Werkzeug.jpg" class="modal-image" alt="Angebot">
-                    </div>
-
-                    <form class="modal-booking-window">
-                        <h3 class="modal-booking-heading">Nur noch ein Schritt!</h3>
-                        
-                        <p class="modal-booking-text">Gesamtbetrag: 1000 €<br>
-                            <input type="checkbox" name="bierZahlung">
-                            <label for="bierZahlung"> Ich möchte in Bier bezahlen (1000 Bier)</label><br>
-                        </p>
-
-                        <button type="button" class="submitBooking">Jetzt buchen</button>
-                    </form>
-
-
-
-                </div>
-                <div class="modal-footer"></div>
-            </div>
-
-        </div>
     </div>
 
     <?php include "PHP/footer.php"; ?>
