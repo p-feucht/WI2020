6f2d5359a4ba57bf5690b40e07d4d53215c57392
kmatysova <kmatysova2@gmail.com>
2020-11-05 22:29:04 +0100

    added empty blog page

--- Teams/Worker-Bees/PHP/header.php
+++ Teams/Worker-Bees/PHP/header.php
@@ -18,13 +18,13 @@
         <div class="header-content-middle">
             <a class="biete" href="FormularBiete.php">Ich biete</a>
             <a href="categories.php">Angebote</a>
-            <a href="#tips">Tipps & Tricks</a>
+            <a href="blog.php">Tipps & Tricks</a>
             <a class="aboutus" href="aboutUs.php">Über uns</a>
             <a href="#contact">Impressum</a>
         </div>
 
         <div class="header-content-right">
             <a href="login.php" class="headerButton">Anmelden</a>
-            <a href="categories.php#Werkstatt-Ang" class="headerButton" id="headerSearch" ><i class="fa fa-search"></i></a>
+            <a href="categories.php" class="headerButton" id="headerSearch" ><i class="fa fa-search"></i></a>
         </div>
     </div>
---
+++ Teams/Worker-Bees/blog.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Worker Bees</title>
+    <meta name="description" content="">
+    <link rel="icon" href="images/logoBiene.png" />
+    <link href="CSS/blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <?php include "PHP/header.php"; ?>
+
+    
+    <div class="blog-content">
+    <h1>Coming Soon...</h1>
+        This will be a place to share ideas and tipps with others.
+        We want to create a community that you can turn to and where you can support others.
+        Stay tuned for amazing updates to this young website! 
+</div>
+
+    <?php include "PHP/footer.php"; ?>
+
+</body>
+
+</html>
