d863ec5f2589adbc627bfed4690b03352124390e
kmatysova <kmatysova2@gmail.com>
2020-11-09 22:33:35 +0100

    changed card label names to be card specific

--- Teams/Worker-Bees/PHP/submitBooking.php
+++ Teams/Worker-Bees/PHP/submitBooking.php
@@ -1,11 +1,11 @@
 <?php
-//include 'header.php';
+
 // good example can be found here: https://www.cloudways.com/blog/custom-php-mysql-contact-form/
 
 //error-variables for validation
-$titleErr = $zeitraumErr = $beschreibErr = $vornameErr = $nachnameErr = $strasseErr = $hnrErr = $plzErr = $ortErr = $preisProTagErr = $preisBetragErr = "";
-$title = $beschreibung = $zeitraum = $vorname = $nachname = $strasse = $hnr = $plz= $ort = $preisProTag1 = $preisProTag2 = $preisBetrag = $a1_bohr = $a2_drechsel = $a3_schleif = $a4_saege = $a5_kleinteil = "";
-$valid=TRUE;
+//$titleErr = $zeitraumErr = $beschreibErr = $vornameErr = $nachnameErr = $strasseErr = $hnrErr = $plzErr = $ortErr = $preisProTagErr = $preisBetragErr = "";
+//$title = $beschreibung = $zeitraum = $vorname = $nachname = $strasse = $hnr = $plz= $ort = $preisProTag1 = $preisProTag2 = $preisBetrag = $a1_bohr = $a2_drechsel = $a3_schleif = $a4_saege = $a5_kleinteil = "";
+//$valid=TRUE;
 
 //Connection to bplaced server
 $servername = "localhost";
@@ -15,17 +15,21 @@ $dbname = "workerbees_db1";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {//wenn auf Submit gedrückt führe Folgendes aus
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
      // Check connection
-     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-        echo "Connection failed";
+     // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
         
-    }
-    else {
+    } else {
 
-        $sql = "";
+        $card_id = $conn->real_escape_string($_POST["cardID"]);
+
+        $pricename = "WZprice_".(string)$card_id;
+        $datename = "WZdate_".(string)$card_id;
+        $titlename = "WZtitel_".(string)$card_id;
+        $bezinbier = "WZbib_".(string)$card_id;
         
         $buchung_id = "";
         $angebot_id = "";
@@ -34,10 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//wenn auf Submit gedrückt führe Fo
         $emailErstell = "";
         $userBuch = "";
         $emailBuch ="";
-        $date = $conn->real_escape_string($_POST["Datum"]);
-        $preis = $conn->real_escape_string($_POST["Preis"]);
-        $titel = $conn->real_escape_string($_POST["angebotTitel"]);
-        $bezinbier = $conn->real_escape_string($_POST["bierZahlung"]);
+        $date = $conn->real_escape_string($_POST[$datename]);
+        $preis = $conn->real_escape_string($_POST[$pricename]);
+        $title = $conn->real_escape_string($_POST[$titlename]);
+        $bezinbier = $conn->real_escape_string($_POST[$bezinbier]);
 
 
         $sql = "INSERT INTO Buchung (Buchung_ID, Angebot_ID, Angebot_Typ, userErstell, emailErstell, userBuch, emailBuch, Preis, angebotTitel, bezInBier)
@@ -49,25 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//wenn auf Submit gedrückt führe Fo
         if ($conn->query($sql) === TRUE) {
 
             echo "New record created successfully";
-            echo "<br>";
-            echo "Entries written:";
-            echo $buchung_id;
-            echo "<br>";
-            echo $angebot_id;
-            echo "<br>";
-            echo $angebot_typ;
-            echo "<br>";
-            echo $userErstell;
-            echo "<br>";
-            echo $emailErstell;
-            echo "<br>";
-            echo $userBuch;
-            echo "<br>";
-            echo $emailBuch;
-            echo "<br>";
-            echo $preis;
-            echo "<br>";
-            echo $bezinbier;
+           
 
             } else {
             echo '<script type="text/javascript">alert("Es tut uns Leid, das Angebot konnte nicht in die Datenbank aufgenommen werden.");</script>';
--- Teams/Worker-Bees/PHP/werkzeugOffers.php
+++ Teams/Worker-Bees/PHP/werkzeugOffers.php
@@ -36,11 +36,17 @@ if ($result->num_rows > 0) {
 
         $bier = round($price,0);
 
+        $pricename = "WZprice_".(string)$orderID;
+        $datename = "WZdate_".(string)$orderID;
+        $titlename = "WZtitel_".(string)$orderID;
+        $bezinbier = "WZbib_".(string)$orderID;
+
     ?>
         <!-- create card for each offer -->
-        <div class="card" id=<?php echo $card_ID ?> data-toggle="modal" data-target=<?php echo $modal_target ?>>
+        <div class="card" name="cardID" value="<?php echo $orderID ?>" id="<?php echo $card_ID ?>" 
+                data-toggle="modal" data-target=<?php echo $modal_target ?>>
             <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($image); ?>" 
-            alt="Offer Photo" class="offer-image"/>
+            alt="Offer Photo" class="offer-image" onerror="this.onerror=null; this.src='images/Werkzeug.jpg'"/>
             <p class="card-lp"><img src="images/place-icon.svg" alt="location" class="place-icon"> <?php echo $location ?>
                 <span class="price"><?php echo $price ?>€ / Tag</span></p>
             <h2><?php echo $title ?></h2>
@@ -57,7 +63,7 @@ if ($result->num_rows > 0) {
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
-                    <h3 class="modal-offerName" name="Titel"><?php echo $title ?></h3>
+                    <h3 class="modal-offerName" name="<?php echo htmlspecialchars($titlename);?>"><?php echo $title ?></h3>
                     <p class="modal-namelocation"><?php echo $offeruser ?>
                         <img src="images/place-icon.svg" class="place-icon" alt="location"><?php echo $plz ?> <?php echo $location ?>
                         <!--has to be the exact location here!-->
@@ -71,21 +77,21 @@ if ($result->num_rows > 0) {
                     </div>
                     <div class="modal-content-split">
                         <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($image); ?>" 
-                            alt="Offer Photo" class="modal-image"/>
+                            alt="Offer Photo" class="modal-image" onerror="this.onerror=null; this.src='images/Werkzeug.jpg'"/>
                     </div>
 
-                    <?php include ("PHP/submitBooking.php"); ?>
+                    <?php include ("./PHP/submitBooking.php"); ?>
 
                     <form class="modal-booking-window" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                         <h3 class="modal-booking-heading">Nur noch ein Schritt!</h3>
                         
                             <label for="bookingDate">Datum:</label>
-                            <input type="date" id="bookingDate" name = "Datum" value="<?php echo htmlspecialchars($date);?>" required><br>
+                            <input type="date" id="bookingDate" name = "<?php echo htmlspecialchars($datename);?>" value="<?php echo htmlspecialchars($date);?>" required><br>
 
-                            <p class="modal-booking-text">Gesamtbetrag: <em name="Preis"><?php echo $price ?></em> €<br></p>
+                            <p class="modal-booking-text">Gesamtbetrag: <em name="<?php echo htmlspecialchars($pricename);?>"><?php echo $price ?></em> €<br></p>
 
-                            <input id="bierInput" type="checkbox" name="bierZahlung">
-                            <label id="bierLabel" for="bierZahlung"> Ich möchte in Bier bezahlen (<?php echo $bier ?> Bier)</label><br>
+                            <input id="bierInput" type="checkbox" name="<?php echo htmlspecialchars($bezinbier);?>">
+                            <label id="bierLabel" for="<?php echo htmlspecialchars($bezinbier);?>"> Ich möchte in Bier bezahlen (<?php echo $bier ?> Bier)</label><br>
                                 <script>
                                     if(<?php echo $bezBier ?> != 1) {
                                         document.getElementById("<?php echo $modal_ID ?>").querySelector("#bierLabel").style.display = "none";
@@ -98,7 +104,7 @@ if ($result->num_rows > 0) {
                                 <option value="inPerson">vor Ort bezahlen</option>
                             </select>
 
-                        <button type="button" class="submitBooking">Jetzt buchen</button>
+                        <button type="submit" class="submitBooking">Jetzt buchen</button>
                     </form>
 
 
