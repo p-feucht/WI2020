ec84992c327d583f72baf0d0f0b718b6350d8c0f
kmatysova <kmatysova2@gmail.com>
2020-11-09 20:44:00 +0100

    improved booking

--- Teams/Worker-Bees/PHP/submitBooking.php
+++ Teams/Worker-Bees/PHP/submitBooking.php
@@ -17,12 +17,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//wenn auf Submit gedrückt führe Fo
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    
-
-
-
+     // Check connection
+     if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        echo "Connection failed";
+        
     }
+    else {
+
+        $sql = "";
+        
+        $buchung_id = "";
+        $angebot_id = "";
+        $angebot_typ = "";
+        $userErstell = "";
+        $emailErstell = "";
+        $userBuch = "";
+        $emailBuch ="";
+        $date = $conn->real_escape_string($_POST["Datum"]);
+        $preis = $conn->real_escape_string($_POST["Preis"]);
+        $titel = $conn->real_escape_string($_POST["angebotTitel"]);
+        $bezinbier = $conn->real_escape_string($_POST["bierZahlung"]);
+
+
+        $sql = "INSERT INTO Buchung (Buchung_ID, Angebot_ID, Angebot_Typ, userErstell, emailErstell, userBuch, emailBuch, Preis, angebotTitel, bezInBier)
+                            VALUES ($buchung_id, $angebot_id, $angebot_typ, $userErstell, $emailErstell, $userBuch, $emailBuch, $preis, $bezinbier)";
     
+        echo $sql;
+        echo "<br>";
+    
+        if ($conn->query($sql) === TRUE) {
+
+            echo "New record created successfully";
+            echo "<br>";
+            echo "Entries written:";
+            echo $buchung_id;
+            echo "<br>";
+            echo $angebot_id;
+            echo "<br>";
+            echo $angebot_typ;
+            echo "<br>";
+            echo $userErstell;
+            echo "<br>";
+            echo $emailErstell;
+            echo "<br>";
+            echo $userBuch;
+            echo "<br>";
+            echo $emailBuch;
+            echo "<br>";
+            echo $preis;
+            echo "<br>";
+            echo $bezinbier;
+
+            } else {
+            echo '<script type="text/javascript">alert("Es tut uns Leid, das Angebot konnte nicht in die Datenbank aufgenommen werden.");</script>';
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            
+            }
+}
+    $conn->close();
+}
     
 ?>
 
--- Teams/Worker-Bees/PHP/werkzeugOffers.php
+++ Teams/Worker-Bees/PHP/werkzeugOffers.php
@@ -28,6 +28,7 @@ if ($result->num_rows > 0) {
         $plz = $row["PLZ"];
         $description = $city = $row["ABeschreibung"];
         $bezBier = $row["BezInBier"];
+        $image = $row["Bild"];
 
         $card_ID = "WZcard_".(string)$orderID;
         $modal_target = "#WZmodal_".(string)$orderID;
@@ -38,7 +39,8 @@ if ($result->num_rows > 0) {
     ?>
         <!-- create card for each offer -->
         <div class="card" id=<?php echo $card_ID ?> data-toggle="modal" data-target=<?php echo $modal_target ?>>
-            <img src="images/werkstatt.jpg" alt="Denim Jeans" class="offer-image">
+            <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($image); ?>" 
+            alt="Offer Photo" class="offer-image"/>
             <p class="card-lp"><img src="images/place-icon.svg" alt="location" class="place-icon"> <?php echo $location ?>
                 <span class="price"><?php echo $price ?>€ / Tag</span></p>
             <h2><?php echo $title ?></h2>
@@ -51,11 +53,11 @@ if ($result->num_rows > 0) {
             <!-- Modal content-->
             <div class="modal-content">
                 <div class="modal-header">
-                    <img class="headerLogo" src="images/logoKomplett.png">
+                    <img class="headerLogo" src="images/logoKomplett.png" alt="Logo">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
-                    <h3 class="modal-offerName"><?php echo $title ?></h3>
+                    <h3 class="modal-offerName" name="Titel"><?php echo $title ?></h3>
                     <p class="modal-namelocation"><?php echo $offeruser ?>
                         <img src="images/place-icon.svg" class="place-icon" alt="location"><?php echo $plz ?> <?php echo $location ?>
                         <!--has to be the exact location here!-->
@@ -65,30 +67,27 @@ if ($result->num_rows > 0) {
                         <p class="offer-description">
                             <?php echo $description ?>
                         </p>
-                        <!--only for Werkstat!!
-                        <ul class="modal-amenities">
-                            <li>Standbohrmaschine</li>
-                            <li>elektrische Standsägen</li>
-                        </ul>
-                        -->
+
                     </div>
                     <div class="modal-content-split">
-                        <img src="images/Werkzeug.jpg" class="modal-image" alt="Angebot">
+                        <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($image); ?>" 
+                            alt="Offer Photo" class="modal-image"/>
                     </div>
 
-                    <form class="modal-booking-window" action="PHP/submitBooking.php" method="post">
+                    <?php include ("PHP/submitBooking.php"); ?>
+
+                    <form class="modal-booking-window" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                         <h3 class="modal-booking-heading">Nur noch ein Schritt!</h3>
                         
                             <label for="bookingDate">Datum:</label>
-                            <input type="date" id="bookingDate"><br>
+                            <input type="date" id="bookingDate" name = "Datum" value="<?php echo htmlspecialchars($date);?>" required><br>
 
-                            <p class="modal-booking-text">Gesamtbetrag: <?php echo $price ?> €<br></p>
+                            <p class="modal-booking-text">Gesamtbetrag: <em name="Preis"><?php echo $price ?></em> €<br></p>
 
                             <input id="bierInput" type="checkbox" name="bierZahlung">
                             <label id="bierLabel" for="bierZahlung"> Ich möchte in Bier bezahlen (<?php echo $bier ?> Bier)</label><br>
                                 <script>
-                                    var bezBier = <?php echo $bezBier ?>;
-                                    if(bezBier!=1) {
+                                    if(<?php echo $bezBier ?> != 1) {
                                         document.getElementById("<?php echo $modal_ID ?>").querySelector("#bierLabel").style.display = "none";
                                         document.getElementById("<?php echo $modal_ID ?>").querySelector("#bierInput").style.display = "none";
                                     }
