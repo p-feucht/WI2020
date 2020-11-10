5db0ac4a66071e3eb80953a0880a6a5b0822fd36
Birk <daniel.birk@boehringer-ingelheim.com>
2020-11-10 21:00:00 +0100

    login

--- Teams/Worker-Bees/login.php
+++ Teams/Worker-Bees/login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password FROM user where username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -43,6 +43,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             
             // Set parameters
             $param_username = $username;
+           /*  $hashed_password = mysqli_query($link, "SELECT password FROM user where username = $username");
+            echo $hashed_password; */
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -52,7 +54,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
+                    /* $hashed_password = "SELECT password FROM user WHERE username = $username";
+                    echo $hashed_password; */
+                    
+                    
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    /* echo $username;
+                    echo ' ,'; 
+                    echo $hashed_password;
+                    echo ' ,'; 
+                    echo $password; */
+                   
+
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
