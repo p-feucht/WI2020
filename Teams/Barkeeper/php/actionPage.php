c03a09d0900cb5cc0a57ddae8466b03ab66b48fd
siggn <niklas.sigg0804@gmail.com>
2020-11-11 15:45:59 +0100

    Abgabe der eigenen Rezeptidee implementiert

--- Teams/Barkeeper/php/actionPage.php
+++ Teams/Barkeeper/php/actionPage.php
@@ -1,3 +1,154 @@
 <?php
+$username = "barkeeper_dbuser";
+$password = "JcNgS55uY2jQJK4a";
+$dbname = "barkeeper_cocktails";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+else {
+    echo "Connected successfully";
+    echo "<br>";
     echo("We have received your data");
+    echo "<br>";
+    $name = $conn->real_escape_string($_POST["CocktailName"]);
+    $beschreibung = $conn->real_escape_string($_POST["Beschreibung"]);
+    $alkohol1 = $conn->real_escape_string($_POST["Alkohol1"]);
+    $alkohol2 = $conn->real_escape_string($_POST["Alkohol2"]);
+    $saft1 = $conn->real_escape_string($_POST["Saft1"]);
+    $saft2 = $conn->real_escape_string($_POST["Saft2"]);
+    $grünzeug1 = $conn->real_escape_string($_POST["Grünzeug1"]);
+    $grünzeug2 = $conn->real_escape_string($_POST["Grünzeug2"]);
+//---------------------------Create cocktail entry----------------------------------------------------
+    $sql = "INSERT INTO cocktails_t (Name, Beschreibung)
+    VALUES ('$name', '$beschreibung')";
+    
+    echo $sql;
+    echo "<br>";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Cocktail sucessfully created";
+        echo "<br>";
+        echo "Entries written:";
+        echo $name;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    //---------------------------Create receipt for Alkohol1----------------------------------------------------
+    $menge = $conn->real_escape_string($_POST["Alkohol1Menge"]);
+    $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+	VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+           '$menge','$alkohol1');";
+    
+    echo $sql;
+    echo "<br>";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Receipt sucessfully created";
+        echo "<br>";
+        echo "Entries written:";
+        echo $name;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+     //---------------------------Create receipt for Alkohol2----------------------------------------------------
+     $menge = $conn->real_escape_string($_POST["Alkohol2Menge"]);
+     $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+     VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+            '$menge','$alkohol2');";
+     
+     echo $sql;
+     echo "<br>";
+ 
+     if ($conn->query($sql) === TRUE) {
+         echo "Receipt sucessfully created";
+         echo "<br>";
+         echo "Entries written:";
+         echo $name;
+     } else {
+         echo "Error: " . $sql . "<br>" . $conn->error;
+     }
+ 
+     //---------------------------Create receipt for Saft1----------------------------------------------------
+     $menge = $conn->real_escape_string($_POST["Saft1Menge"]);
+     $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+     VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+            '$menge','$saft1');";
+     
+     echo $sql;
+     echo "<br>";
+ 
+     if ($conn->query($sql) === TRUE) {
+         echo "Receipt sucessfully created";
+         echo "<br>";
+         echo "Entries written:";
+         echo $name;
+     } else {
+         echo "Error: " . $sql . "<br>" . $conn->error;
+     }
+
+      //---------------------------Create receipt for Saft2----------------------------------------------------
+    $menge = $conn->real_escape_string($_POST["Saft2Menge"]);
+    $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+	VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+           '$menge','$saft2');";
+    
+    echo $sql;
+    echo "<br>";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Receipt sucessfully created";
+        echo "<br>";
+        echo "Entries written:";
+        echo $name;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+  //---------------------------Create receipt for Grünzeug1----------------------------------------------------
+  $menge = $conn->real_escape_string($_POST["Grünzeug1Menge"]);
+  $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+  VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+         '$menge','$grünzeug1');";
+  
+  echo $sql;
+  echo "<br>";
+
+  if ($conn->query($sql) === TRUE) {
+      echo "Receipt sucessfully created";
+      echo "<br>";
+      echo "Entries written:";
+      echo $name;
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+
+   //---------------------------Create receipt for Grünzeug2----------------------------------------------------
+   $menge = $conn->real_escape_string($_POST["Grünzeug2Menge"]);
+   $sql = "INSERT INTO Rezept_RT (cocktails_id, menge, zutat)
+   VALUES((SELECT MAX(cocktails_id) FROM cocktails_t),
+          '$menge','$grünzeug2');";
+   
+   echo $sql;
+   echo "<br>";
+
+   if ($conn->query($sql) === TRUE) {
+       echo "Receipt sucessfully created";
+       echo "<br>";
+       echo "Entries written:";
+       echo $name;
+   } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+   }
+
+
+    $conn->close();    
+}
 ?>
--- Teams/Barkeeper/rezeptidee.php
+++ Teams/Barkeeper/rezeptidee.php
@@ -11,7 +11,88 @@
         ?>
     </nav>
     <div class="content">
-        <p>Hier kann der User seine Rezeptidee einreichen</p>
+    <form action = "/php/actionPage.php" method = "Post" id ="Input_Ingredients">
+            <input 
+                type="text" name="CocktailName" placeholder = "Cocktail Name" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Cocktail Name'" 
+            />
+            <input 
+                type="text" name="Beschreibung" placeholder = "Kurze Beschreibung" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='KurzeBeschreibung'" 
+            />
+            <br>
+            <input 
+                type="text" name="Alkohol1" placeholder = "Alkohol" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Alkohol'" 
+            />
+            <input 
+                type="text" name="Alkohol1Menge" placeholder = "Menge" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            />
+            <br>
+            <input 
+                type="text" name="Alkohol2" placeholder="Alkohol"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Alkohol'" 
+            >
+            <input 
+                type="text" name="Alkohol2Menge" placeholder = "Menge" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            />
+            <br>
+            <input 
+                type="text" name="Saft1" placeholder="Saft"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Saft'" 
+            > 
+            <input 
+                type="text" name="Saft1Menge" placeholder = "Menge" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            />
+            <br>
+            <input 
+                type="text" name="Saft2" placeholder="Saft"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Saft'" 
+            > 
+            <input 
+                type="text" name="Saft2Menge" placeholder = "Menge" 
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            />
+            <br>
+            <input 
+                type="text" name="Grünzeug1" placeholder="Grünzeug"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Grünzeug'" 
+            >
+            <input 
+                type="text" name="Grünzeug1Menge" placeholder="Menge"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            >
+            <br>
+            <input 
+                type="text" name="Grünzeug2" placeholder="Grünzeug"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Grünzeug'" 
+            >
+            <input 
+                type="text" name="Grünzeug2Menge" placeholder="Menge"
+                onfocus="this.placeholder=''" 
+                onblur="this.placeholder='Menge'" 
+            >
+            <br>
+            <input 
+                type="submit" value="Absenden"
+            >
+        </form>
     </div>
     <footer>
     <?php
