2413975fbace21cfba8fc2d0c3ce895eba7bcba6
Nico Albrecht <nicolai.albrecht96@gmail.com>
2020-11-13 13:51:06 +0100

    Tipps

--- Teams/Barkeeper/tipps.php
+++ Teams/Barkeeper/tipps.php
@@ -11,7 +11,26 @@
         ?>
     </nav>
     <div class="content">
-        <p>Hier werden Tipps angezeigt</p>
+        <p>Da wir alle erstmal lernen müssen wie man denn den perfekten Cocktail von zu Hause aus mixt,
+         hier mal ein paar unserer Tipps und Tricks mit welchen es auf jeden Fall klappt</p>
+        <p>Und mach dir keine Sorge - Übung macht den Meister!</p>
+
+        <p>Tipp 1: 
+        Zum Cocktails mixen eignet sich natürlich am Besten ein sogenannter Cocktail shaker.
+        Den findest du in jedem Haushaltswarengeschäft oder auf Amazon etc. Aber diesen richtig zu benutzen ist gar nicht sooo einfach,
+        deswegen hier ein kleines Tutorial für dich:</p>
+        <a href="https://www.youtube.com/watch?v=AUAJE4UrZbE">Cocktail Shaken für Anfänger</a>
+        <p>Und solltest du noch einen Shaker gebrauchen, haben wir auch schon einen für dich gefunden:</p>
+        <a href="https://www.barstuff.de/barzubehoer-cocktail-sets/bar-aid-set-classic-home.html">Dein Cocktail Shaker</a>
+        <p>Tipp 2:
+        Cocktails sollten nicht warm serviert werden, deswegen schau darauf, dass auch dein Cocktail Glas schön kühl ist.
+        Stelle es entweder ein paar Minuten vorher in den Kühlschrank oder gib schon zu Beginn Eis hinein.</p>
+        <p>Tipp 3:
+        Man sagt nicht umsonst das Auge trinkt mit, deshalb solltest du auch auf das Äußere deines Cocktails achten.
+        Das geht ganz leicht, in dem du entweder eine Frucht schön aufschneidest und an den Glasrand steckst, oder in 
+        dem du etwas "Grünzeug" wie Minze auf den Cocktail legst</p>
+        <p>Rezept 4</p>
+        <p>Rezept 5</p>       
     </div>
     <footer>
         <?php
