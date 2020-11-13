5ed37f6ed688a345ddacd76b74731894d55d0a15
Birk <daniel.birk@boehringer-ingelheim.com>
2020-11-13 13:49:43 +0100

    loginohnetest

--- Teams/Worker-Bees/login.php
+++ Teams/Worker-Bees/login.php
@@ -87,7 +87,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: indexTest.php");
+                            header("location: index.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "Das eingegebene Passwort ist falsch.";
