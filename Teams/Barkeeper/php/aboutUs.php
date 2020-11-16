df7e92ef64064619faa39954b4f1a1fc2f949a19
Nico Albrecht <nicolai.albrecht96@gmail.com>
2020-11-16 13:23:04 +0100

    Icons

--- Teams/Barkeeper/php/aboutUs.php
+++ Teams/Barkeeper/php/aboutUs.php
@@ -1,2 +1,3 @@
-<a href="impressum.php" class="button">Impressum</a>
-<a href="kontakt.php" class="button">Kontakt</a>
+<a href="impressum.php" class="button">Impressum <img src="images/impressum.jpg"></a>
+<a href="kontakt.php" class="button">Kontakt <img src="images/Contact.jpg"></a>
+
