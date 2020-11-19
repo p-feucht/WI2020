170ab2c03446d1d0f324c726fb8dd84d4b969060
philipp-code <schneider.langenargen@gmail.com>
2020-11-19 18:55:41 +0100

    progress

---
+++ Teams/RUADataScientist/js/Carousel.js
@@ -0,0 +1,36 @@
+$(function () {
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    dots: false,
+    centerMode: true,
+    centerPadding: '0',
+    pauseOnHover: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    mobileFirst: true,
+    reponsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+      }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          arrows: false
+        }
+      }, {
+        breakpoint: 300,
+        settings: "unslick"
+      }]
+  });
+});
+
