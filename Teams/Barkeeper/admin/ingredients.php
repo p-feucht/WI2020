8c4ba865ed0aa1ecd71712637566c1e9421687ff
siggn <niklas.sigg0804@gmail.com>
2020-11-26 17:26:50 +0100

    Weitere Verschönerungen

--- Teams/Barkeeper/admin/ingredients.php
+++ Teams/Barkeeper/admin/ingredients.php
@@ -31,7 +31,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Barkeeper | Admin Zutaten</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 </head>
 
--- Teams/Barkeeper/admin/own-result.php
+++ Teams/Barkeeper/admin/own-result.php
@@ -58,7 +58,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barkeeper | Dein Cocktail</title>
+    <title>Barkeeper | Admin Cocktail bearbeitet</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="/assets/css/styles.css">
